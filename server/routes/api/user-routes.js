3ceb2a6aed25ae89eec519084c9d8c3932cb5edf
Jacob Owen <jowenlcjvs@gmail.com>
2022-07-19 20:42:06 -0400

    schema folder added, files changed in client src 07/19/22

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -5,18 +5,20 @@ const {
   saveBook,
   deleteBook,
   login,
+  deleteMovie,
+  saveMovie,
 } = require('../../controllers/user-controller');
 
 // import middleware
 const { authMiddleware } = require('../../utils/auth');
 
 // put authMiddleware anywhere we need to send a token for verification of user
-router.route('/').post(createUser).put(authMiddleware, saveBook);
+router.route('/').post(createUser).put(authMiddleware, saveMovie);
 
 router.route('/login').post(login);
 
 router.route('/me').get(authMiddleware, getSingleUser);
 
-router.route('/books/:bookId').delete(authMiddleware, deleteBook);
+router.route('/movies/:movieId').delete(authMiddleware, deleteMovie);
 
 module.exports = router;
