dc08d609f1d42065b7e3b2cc0424f6393c07fe62
mohamed behnas <behna@DESKTOP-T1K4JBI.(none)>
2022-07-22 10:29:44 -0400

    edit queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,18 +1,51 @@
 import gql from 'graphql-tag';
 
-export const QUERY_ME = gql`
+export const GET_USER = gql`
   {
     me {
       _id
       username
       email
-      savedMovies {
-        movieId
-        authors
-        image
-        description
+      friends {
+        _id
+      }
+      likedMovies{
+        _id
+        externalMovieId
+        rating
+        voteCount
         title
-        link
+        overview
+        releaseDate
+        poster
+        trailer
+        likedUsers {
+          _id
+          username
+        }
+        dislikedUsers {
+          _id
+          username
+        }
+      }
+      dislikedMovies{
+        _id
+        externalMovieId
+        rating
+        voteCount
+        title
+        overview
+        releaseDate
+        poster
+        trailer
+        likedUsers {
+          _id
+          username
+        }
+        dislikedUsers {
+          _id
+          username
+        }
       }
     }
   }
