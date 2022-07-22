7b0f71e02db800061918891bfe87a86d1d78c490
nbross <nbross2001@gmail.com>
2022-07-22 16:09:07 -0400

    changed book.js to movie.js

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -1,6 +1,6 @@
 const { Schema } = require('mongoose');
 
-// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedMovies` array in User.js
+// This is a sub-document schema, it won't become its own model but we'll use it as the schema for the User's `savedMovies` array in User.js
 const movieSchema = new Schema({
   overview: {
     type: String,
--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
 // import schema from Book.js
-const movieSchema = require('./Book');
+const movieSchema = require('./Movie');
 
 const userSchema = new Schema(
   {
