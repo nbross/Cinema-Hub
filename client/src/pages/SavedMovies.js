21912401588263000b8b75dbe643b16cb667dcd6
Jacob Owen <jowenlcjvs@gmail.com>
2022-07-25 20:48:48 -0400

    nothing

--- client/src/pages/SavedMovies.js
+++ client/src/pages/SavedMovies.js
@@ -52,7 +52,7 @@ const SavedMovies = () => {
     }
 
     try {
-      const { data } = await watchedMovie({
+      const { data } = await watchedMovi({
         variables: { movieId },
       });
 
