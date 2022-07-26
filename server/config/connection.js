1f90d1dff1e60937946b0864eaee49ece3725a0f
mohamed behnas <behna@DESKTOP-T1K4JBI.(none)>
2022-07-26 19:01:47 -0400

    updating the readme files

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cinema-hub', {
-=======
 mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Cinema-Hub", {
->>>>>>> addbcfa5484db271753949bb4f2b83ee41ea00b1
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
